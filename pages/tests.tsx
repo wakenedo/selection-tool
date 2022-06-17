850ae35f04f6437f307e4aa69e306c83ed4d792a
wakenedo <a_nuner@outlook.com>
2022-06-17 13:34:54 -0300

    Applying Roboto Font, and MUI button

--- pages/tests.tsx
+++ pages/tests.tsx
@@ -1,21 +1,24 @@
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
+import {Button} from '@mui/material'
 
-const Tests : NextPage = () => {
-    const [counter , setCounter] = useState(0)
+const Tests: NextPage = () => {
+    const [counter, setCounter] = useState(0)
 
     const handleCounterIteration = () => setCounter(counter + 1)
 
     useEffect(() => {
         document.title = 'This counter result' + counter + '!'
-     })
+    })
 
 
     return (
         <div>
             <h1>Lets Count <span>{counter}</span></h1>
-            <button onClick={e => setCounter(0)}>Reset</button> 
-            <button onClick={handleCounterIteration}>Iterate</button>
+            <div>
+                <Button onClick={e => setCounter(0)}>Reset</Button>
+                <Button onClick={handleCounterIteration}>Iterate</Button>
+            </div>
         </div>
     )
 }
