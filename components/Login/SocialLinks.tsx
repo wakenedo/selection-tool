57dd5d1dbd139879ac39f50efde30b33aafad5f5
wakenedo <a_nuner@outlook.com>
2022-07-13 01:34:40 -0300

    Update SocialLinks.tsx

--- components/Login/SocialLinks.tsx
+++ components/Login/SocialLinks.tsx
@@ -1,18 +1,25 @@
 import { Box, Icon, Stack } from '@mui/material'
 import React from 'react'
+import Image from 'next/image'
 
 //Assets
 import LinkedIn from '../../assets/SocialMediaIcons/iconscout.com/icon/linkedin.png'
 // https://iconscout.com/icon/github-159
 import GitHub from '../../assets/SocialMediaIcons/iconscout.com/icon/github.png'
 
+
 const SocialLinks = () => {
   return (
     <Stack
     direction='row'
     alignItems='center'
+    margin='20px'
+    padding='20px'
+    
     >
-      
+      <Image  src={LinkedIn} />
+      <Image src={GitHub} />
+
     </Stack>
   )
 }
